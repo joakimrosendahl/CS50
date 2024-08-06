2eee25aa69f9c0472acf2a57d5161e08e1ee5f09
Joakim <joakim@Dr.Sten>
2024-08-06 21:05:33 +0200

    Initial commit

---
+++ calculator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int main(void)
+{
+    int counter = 0;
+    printf("i is: %i\n", counter);
+    while (counter < 3)
+    {
+        printf("i equals to: %i\n", counter);
+        counter ++;        
+    }
+}
