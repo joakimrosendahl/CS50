1afe44b3b6bd51ed984728a1f7d6be6baf25c79f
Joakim <joakim@Dr.Sten>
2024-08-07 10:55:33 +0200

    Discount example

---
+++ discount.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+float discount(float price, int percent_off);
+
+int main(void)
+{
+    float regular = get_float("Regular Price: ");
+    int percent_off = get_int("Percent off: ");
+    float sale = discount(regular, percent_off);
+    printf("Sale Price: %.2f\n", sale);
+}
+
+
+float discount(float price, int percent_off)
+{
+    return price * (100 - percent_off) * .01;
+}
